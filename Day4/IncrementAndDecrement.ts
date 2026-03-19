eabcce645dd6f6c57df7c8b786420fbdc2504f7b
mobileautomationuser42 <mobileautomationuser42@gmail.com>
2026-03-19 15:29:52 +0530

    MOD

--- Day4/IncrementAndDecrement.ts
+++ Day4/IncrementAndDecrement.ts
@@ -32,18 +32,18 @@ console.log(x);   // 11
 // exp ? res1 : res2;
 
 // Example 1
-/*
+
 let a: number = 100, b: number = 200;
 
 let res: number = (a > b) ? a : b;
 console.log(res); // 200
-*/
+
 
 // Example 2:
 
 let personAge: number = 30;
 
 // Ternary operator: condition ? valueIfTrue : valueIfFalse
-let res: string;
-res = (personAge >= 18) ? "Adult" : "Minor";
-console.log(res); // Adult
+// let res: string;
+// res = (personAge >= 18) ? "Adult" : "Minor";
+// console.log(res); // Adult
